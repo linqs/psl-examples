4aa349e503069b63df375d9e492b1e30fc14df62
Eriq Augustine <eriq.augustine@gmail.com>
2017-12-07 13:44:00 -0800

    Added the CLI version of the simple-acquaintances (simple-lp) from the old examples repo.

---
+++ simple-acquaintances/README.md
@@ -0,0 +1,21 @@
+### Simple Acquaintances
+
+## Problem
+
+In this example, we attempt to predict acquaintance links between different people.
+This problem is made to be very small and is great for when you want to dig into the detail of what PSL is doing,
+since it would not be infeasible to write out all the grounds/potentials.
+
+## Dataset
+
+The dataset is synthetic and small.
+It contains information on where people have lived, what activities they like/dislike, and some already observed acquaintance links.
+
+## Keywords
+
+ - `cli`
+ - `evaluation`
+ - `inference`
+ - `link prediction`
+ - `small scale`
+ - `synthetic data`
---
+++ simple-acquaintances/cli/run.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+readonly JAR_URL='https://linqs-data.soe.ucsc.edu/maven/repositories/psl-releases/org/linqs/psl-cli/CANARY/psl-cli-CANARY.jar'
+readonly JAR_PATH='psl-cli-CANARY.jar'
+readonly BASE_NAME='simple-acquaintances'
+
+readonly ADDITIONAL_EVAL_OPTIONS='--eval-discrete 0.5'
+
+function main() {
+   trap exit SIGINT
+
+   # The data is already included, no need to fetch it.
+
+   # Make sure we can run PSL.
+   check_requirements
+   fetch_file "${JAR_URL}" "${JAR_PATH}" 'psl-jar'
+
+   # Run PSL
+   runEvaluation
+}
+
+function runEvaluation() {
+   echo "Running PSL Inference"
+
+   java -jar "${JAR_PATH}" -infer -model "${BASE_NAME}.psl" -data "${BASE_NAME}.data" -output eval-results ${ADDITIONAL_EVAL_OPTIONS}
+   if [[ "$?" -ne 0 ]]; then
+      echo 'ERROR: Failed to run infernce'
+      exit 70
+   fi
+}
+
+function check_requirements() {
+   local hasWget
+   local hasCurl
+
+   type wget > /dev/null 2> /dev/null
+   hasWget=$?
+
+   type curl > /dev/null 2> /dev/null
+   hasCurl=$?
+
+   if [[ "${hasWget}" -ne 0 ]] && [[ "${hasCurl}" -ne 0 ]]; then
+      echo 'ERROR: wget or curl required to download the jar'
+      exit 10
+   fi
+
+   type java > /dev/null 2> /dev/null
+   if [[ "$?" -ne 0 ]]; then
+      echo 'ERROR: java required to run project'
+      exit 13
+   fi
+}
+
+function get_fetch_command() {
+   type curl > /dev/null 2> /dev/null
+   if [[ "$?" -eq 0 ]]; then
+      echo "curl -o"
+      return
+   fi
+
+   type wget > /dev/null 2> /dev/null
+   if [[ "$?" -eq 0 ]]; then
+      echo "wget -O"
+      return
+   fi
+
+   echo 'ERROR: wget or curl not found'
+   exit 20
+}
+
+function fetch_file() {
+   local url=$1
+   local path=$2
+   local name=$3
+
+   if [[ -e "${path}" ]]; then
+      echo "${name} file found cached, skipping download."
+      return
+   fi
+
+   echo "Downloading ${name} file with command: $FETCH_COMMAND"
+   `get_fetch_command` "${path}" "${url}"
+   if [[ "$?" -ne 0 ]]; then
+      echo "ERROR: Failed to download ${name} file"
+      exit 30
+   fi
+}
+
+main "$@"
