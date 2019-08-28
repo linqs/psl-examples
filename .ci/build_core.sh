7e074871d5ca0f40b36603d62e142f951ab7f6c1
yinyang <shresta.bs@gmail.com>
2019-08-28 16:49:58 -0700

    Adds CI(continuous integration) support with travis CI to the cli interface of psl-examples to detect build breakages after commits in this repo.

---
+++ .ci/build_core.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -e
+# Build and install the PSL core matching this version.
+BUILD_DIR='/tmp/__building_psl_core__'
+TARGET_REPOS="$@"
+
+if [ $# -eq 0 ]; then
+   echo "No target repositories specified"
+   exit 1
+fi
+
+# If the pushed branch is master, we will use master for core.
+# Otherwise, we will use develop.
+branch='develop'
+if [ "$TRAVIS_BRANCH" = 'master' ]; then
+   branch='master'
+fi
+
+# We will always use the same owner for PSL core as this repo's owner.
+owner=$(echo "$TRAVIS_REPO_SLUG" | sed 's#/.\+$##')
+
+pushd . > /dev/null
+for repo in $TARGET_REPOS; do
+   gitUrl="https://github.com/${owner}/${repo}.git"
+   echo "Building ${gitUrl} (${branch}) ..."
+
+   cd
+   rm -Rf "${BUILD_DIR}"
+   mkdir -p "${BUILD_DIR}"
+   cd "${BUILD_DIR}"
+
+   git clone "${gitUrl}"
+   cd "${repo}"
+   git checkout "${branch}"
+
+   mvn clean install -DskipTests
+done
+popd > /dev/null
+
+find ./*/cli/* -name 'run.sh' -exec dirname {} \; | while read line; do
+    pushd . > /dev/null
+    cd $line;
+    ./run.sh --postgres psltest -D log4j.threshold=DEBUG
+    popd > /dev/null
+done
