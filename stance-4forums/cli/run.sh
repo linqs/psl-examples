ba017283308cd180bca87c4012be7b839d21107b
Eriq Augustine <eriq.augustine@gmail.com>
2019-12-05 15:16:05 -0800

    Updated the version to a snapshot for development.

--- stance-4forums/cli/run.sh
+++ stance-4forums/cli/run.sh
@@ -4,7 +4,7 @@
 # These options are blind-passed to the CLI.
 # Ex: ./run.sh -D log4j.threshold=DEBUG
 
-readonly PSL_VERSION='2.2.0'
+readonly PSL_VERSION='2.3.0-SNAPSHOT'
 readonly JAR_PATH="./psl-cli-${PSL_VERSION}.jar"
 readonly FETCH_DATA_SCRIPT='../data/fetchData.sh'
 readonly BASE_NAME='stance-4forums'
