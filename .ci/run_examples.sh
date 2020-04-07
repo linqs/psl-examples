c033ab28a55ac2d3afa249a8d570fe6bd4b36f24
Eriq Augustine <eriq.augustine@gmail.com>
2020-04-06 17:29:40 -0700

    Removed a log from CI and reduced the number of weight learning iterations.

--- .ci/run_examples.sh
+++ .ci/run_examples.sh
@@ -5,17 +5,9 @@ trap exit SIGINT
 
 # Run all run.sh scripts in psl-examples repo.
 for line in `ls ./*/cli/run.sh` ; do
-    echo "Starting space"
-    df -h
-    du -csh *
-
     dir=$(dirname $line)
     pushd . > /dev/null
         cd $dir
-        ./run.sh --postgres psltest -D log4j.threshold=DEBUG -D votedperceptron.numsteps=2 -D admmreasoner.maxiterations=10
+        ./run.sh --postgres psltest -D log4j.threshold=DEBUG -D votedperceptron.numsteps=1 -D admmreasoner.maxiterations=10
     popd > /dev/null
-
-    echo "Ending space"
-    df -h
-    du -csh *
 done
