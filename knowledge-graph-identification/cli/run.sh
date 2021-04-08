3df8e777c0cf8ece259d9c2915dde2c59714c7c6
Alex M <almumill@ucsc.edu>
2021-04-08 13:45:56 -0700

    Update name of RankingEvaluator to AUCEvaluator

--- knowledge-graph-identification/cli/run.sh
+++ knowledge-graph-identification/cli/run.sh
@@ -11,7 +11,7 @@ readonly BASE_NAME='knowledge-graph-identification'
 
 readonly ADDITIONAL_PSL_OPTIONS='--int-ids'
 readonly ADDITIONAL_LEARN_OPTIONS='--learn'
-readonly ADDITIONAL_EVAL_OPTIONS='--infer --eval RankingEvaluator'
+readonly ADDITIONAL_EVAL_OPTIONS='--infer --eval AUCEvaluator'
 
 function main() {
    trap exit SIGINT
