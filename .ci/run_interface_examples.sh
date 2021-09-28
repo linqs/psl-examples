d075bcb9224d5d3a58fb80e0af71c1641b537ea9
Eriq Augustine <eriq.augustine@gmail.com>
2021-09-28 11:29:40 -0700

    Moving CI from Travis to Github Actions.

---
+++ .ci/run_interface_examples.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Run an instance of each PSL interface.
+
+readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly BASE_DIR="${THIS_DIR}/.."
+
+function main() {
+    if [[ $# -ne 1 ]] || [[ $1 != 'test' && $1 != 'prod' ]] ; then
+        echo "USAGE: $0 <test|prod>"
+        exit 1
+    fi
+
+    trap exit SIGINT
+    set -e
+
+    # TODO(eriq): Soon we will enable all interfaces, but only do CLI now.
+    cd "${BASE_DIR}/simple-acquaintances/cli"
+    ./run.sh --postgres psltest -D log4j.threshold=DEBUG -D votedperceptron.numsteps=1 -D admmreasoner.maxiterations=10
+}
+
+[[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "$@"
